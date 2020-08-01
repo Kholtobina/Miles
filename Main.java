b80fce974d384481d86f0c676f12104a3c6c5733
Yulia Kholtobina <another.z@rambler.ru>
2020-08-01 22:55:43 +0300

    Initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        long priceInKopecks = 1534561;
+        int mile = (int) (priceInKopecks / 2000);
+
+        System.out.println(mile);
+    }
+}
